1c840da1184b037b4bb650bdb3a8269f869f8c72
Eijibm <eijibm@gmail.com>
2022-03-14 22:30:49 -0400

    Flashlight

--- Assets/flashlight.cs
+++ Assets/flashlight.cs
@@ -21,7 +21,7 @@ public class flashlight : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.F))
 		{
 			if (isON)
 			{
